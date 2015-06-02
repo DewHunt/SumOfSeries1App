7a38049e313dbbe9429caea689671d349a137d80
s-sabbir <dew.fog1553@gmail.com>
2015-06-03 00:17:31 +0600

    Initial Commit for SumofSeries1

---
+++ ProblemThreeApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemThreeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Numbers are : ");
+            Console.Write("\n\t");
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.Write(i+", ");
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine("\n\nThe Summation : " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
